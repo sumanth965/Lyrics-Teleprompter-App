7cac897f4d53939c1a94611307540edcc31f1939
sumanth965 <sumanthpoojary965@gmail.com>
2026-06-13 21:03:02 +0530

    Login and Register

--- backend/models/User.js
+++ backend/models/User.js
@@ -26,11 +26,10 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function hashPassword(next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function hashPassword() {
+  if (!this.isModified("password")) return;
 
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 userSchema.methods.comparePassword = function comparePassword(candidatePassword) {
